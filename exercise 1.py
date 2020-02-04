fab65430aee671f41f2734b06b7c7c8424a9d03f
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-04 22:51:10 +0000

    tried loop stuff

--- exercise 1.py
+++ exercise 1.py
@@ -9,16 +9,23 @@ anwser =input('Guess how much points Kobe Bryant has scored in total for LA Lake
 
 ###check if anwser is correct
 #use operator equation and check if anwser is correct
-anwser_difference = points - int(anwser)
+
 
 #boolean as a question
+
+
+anwser_difference = points - int(anwser)
+anwser_difference == 0
 print(anwser_difference == 0)
+i = 0
+
+   i += 1
+if anwser_difference == 0:
+    print(f"You are correct! Kobe bryant scored a total of {actual_points}points between 1996 and 2016.")
+    break
+elif 0 < anwser_difference < 5001:
+    print("so close! try a little bit lower.. ")
+elif -5001 < anwser_difference < 0:
+    print("so close! try a little bit higher.. ")
 
 
-#if anwser_difference == 0:
- #   print(f"You are correct! Kobe bryant scored a total of {actual_points}) between 1996 and 2016")
-#elif 0 < anwser_difference < 5001:
- #   print("so close! try a little bit lower.. ")
-#elif -5001 < anwser_difference < 0:
-#    else print(sorry, not correct!)
-#    end
