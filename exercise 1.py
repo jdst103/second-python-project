2e609f8c07b1c4fb543a802088bd65b4175e3d60
Jason Robinson <jrobinson@spartaglobal.com>
2020-02-05 11:14:20 +0000

    making while loops on python

--- exercise 1.py
+++ exercise 1.py
@@ -5,7 +5,7 @@ actual_points = 33643
 points = 34000
 
 #prompt user for anwser
-anwser =input('Guess how much points Kobe Bryant has scored in total for LA Lakers to the nearest thausand')
+anwser =input('Guess how much points Kobe Bryant has scored in total for LA Lakers to the nearest thausand!!')
 
 ###check if anwser is correct
 #use operator equation and check if anwser is correct
@@ -17,13 +17,17 @@ anwser =input('Guess how much points Kobe Bryant has scored in total for LA Lake
 anwser_difference = points - int(anwser)
 anwser_difference == 0
 print(anwser_difference == 0)
-i = 0   i += 1
-if anwser_difference == 0:
-    print(f"You are correct! Kobe bryant scored a total of {actual_points}points between 1996 and 2016.")
-    break
-elif 0 < anwser_difference < 5001:
-    print("so close! try a little bit lower.. ")
-elif -5001 < anwser_difference < 0:
-    print("so close! try a little bit higher.. ")
+
+while anwser_difference != 0:
+    anwser = input('Oh noo, try again!!')
+    anwser_difference = points - int(anwser)
+    print(anwser_difference == 0)
+
+    if anwser_difference == 0:
+        print(f"You are correct! Kobe bryant scored a total of {actual_points} points between 1996 and 2016.")
+    if 0 < anwser_difference < 5001:
+        print("so close! try a little bit lower.. ")
+    if -5001 < anwser_difference < 0:
+         print("so close! try a little bit higher.. ")
 
 
